1b3792612c2a253c462b09411082faf6a4d0f069
musabosman04 <musabosman3104@gmail.com>
2020-08-30 21:52:00 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,98 @@
+
+// Bangla Paragraph
+var banglaOne = 'যখন সে মাঠে গিয়ে গান গাইত, তখন মাঠের যত গরু সব দড়ি ছিঁড়ে ভাগত। আমাদের গ্রামে এমন একটা ওস্তাদ হয়েছে, তার বাজনাটা বন্ধ হয়ে যাবে!’ সেখানে রাখালের দল লাঠি নিয়ে আসতে বনের ভিতর গিয়ে খুব ক’রে গলা ভাঁজতে লাগল। তাকে দেখেই ভারি আশ্চর্য হয়ে গুপি জিজ্ঞাসা করল, ‘তুমি কে হে?’ ওটা এলেই তো ঢোলকসুদ্ধ আমাকে গিলে খাবে!’';
+
+var banglaTwo = 'গুপিদের গ্রমের কাছেই আরেকটা গ্রামে একজন লোক থাকত, তার নাম ছিল পাঁচু পাইন। গুপিদের গ্রমের কাছেই আরেকটা গ্রামে একজন লোক থাকত, তার নাম ছিল পাঁচু পাইন। ওটা এলেই তো ঢোলকসুদ্ধ আমাকে গিলে খাবে!’';
+
+var banglaThree = 'বেরিয়েই দেখে আর একটা লোকও বিশাল ঢোল মাথায় করে সেই বনের ভিতর থেকে আসছে। শেষে আর তার ভয়ে তার বাবার দোকানে খদ্দেরই আসে না, রাখালেরাও মাঠে গরু নিয়ে যেতে পারে না। গুপি কিনা একটা গান গাইতে পারত, আর সে গ্রামের আর কেউ কিছু গাইতে পারত না, তাই তারা তাকে খাতির করে বলত গুপি ‘গাইন’। এখন আর কেউ তার বাজনা শুনে লাঠি নিয়ে আসে না।';
+
+var banglaFour = 'বাঘা আজও তাকে দেখতে পায়নি, শুধু দূর থেকে তার ডাক শুনে ভয়ে থরথরিয়ে কাঁপে, আর ভাবে, ‘বাবা গো। গুপিও বাঘার বাজনা শুনতে পায়, আর বাঘারই মতো ভয়ে কাঁপে। তোমরা গান গাইতে পার? আমি একজন লোকের কথা বলব, সে একটা গান গাইতে পারত। সে যা ঢোলক হল! তার মুখ হল সাড়ে-তিন হাত চওড়া, আর ছাউনি মোষের চামড়ার। বাঘা সেটা পেয়ে যার পর নাই খুশি হয়ে বললে, ‘আমি দাঁড়িয়ে বাজাব।’ তার নাম ছিল গুপি কাইন, তার বাবার নাম ছিল কানু কাইন। আর তার ঢোলকের আওয়াজ শুনলেই আশপাশের সকল গ্রামের লোক লাঠি নিয়ে আসে। সেখানে দুদিন না থাকতে থাকতেই সেখানকার সকলে মিলে তাকে গ্রাম থেকে বার করে দিল।’';
+
+var banglaFive = 'শেষে আর পাঁচু তার ঢোলকের পয়সা দিয়ে উঠতে পারে না। বাঘা আর কী করে? তার একটা মুদীর দোকান ছিল। এখন বাঘার বেশ মজাই হয়েছে। ততদিনে তার বাজনা শুনে শুনে তার বাপ মা পাগল হয়ে গেল, গ্রামের লোকের মাথা ঘুরতে লাগল।';
+
+
+// Arabic Paragraph
+var arabicOne = 'دار سقطت المشتّتون قد, للصين وبعدما الباهضة ان يتم. شيء إذ بخطوط أثره،, أي بها زهاء التحالف. واتّجه بمعارضة ٣٠ بحق. أهّل للمجهود إذ الى, فرنسا أطراف الأمور أسر أن.';
+
+var arabicTwo = 'أخذ بـ فبعد وبريطانيا, حتى أوسع تاريخ البرية تم. ثم دنو أراض المزيفة, لان كل خطّة معارضة, لإعادة أوروبا إتفاقية قبل في. منتصف بالمحور قام بل, ٣٠ بالرّد حاملات ومن. تصرّف السبب تزامناً يبق ان. عل عرض النفط موالية الساحل, بـ أفاق الهادي حيث. إحتار أوروبا اقتصادية عن غير, هو حين أواخر وأكثرها. عدم عل دأبوا ليركز, لكل ثم جديدة وتتحمّل للأراضي.';
+
+var arabicThree = 'إذ عدد الأجل حاملات وحلفاؤها. تعد من نتيجة أعمال وايرلندا, حادثة البشريةً قد عدم. الأمم ايطاليا، التبرعات دون أي. حيث بتخصيص وإيطالي بـ. حيث كل المارق بالسيطرة.';
+
+var arabicFour = 'حيث ما مئات الحرة إتفاقية, الى شمال للصين لهيمنة من. دول إذ مكّن أعمال الثالث, ٣٠ ببعض الأولية حول, تزامناً لبولندا، إذ حول. مع بعض معاملة ليتسنّى. الجو مسارح ٣٠ حين, بل شيء وعُرفت السادس الانجليزية.';
+
+var arabicFive = 'عل وفنلندا ومطالبة عرض. وجهان قائمة بولندا، بـ تحت, وتم تونس الأعمال ثم. حين يذكر ووصف لم, ٠٨٠٤ ودول الهجوم عدم عن. تم جُل الهادي إتفاقية الفرنسية, الحدود واشتدّت بعد ٣٠. أخذ في لكون إحكام القادة.';
+
+
+
+// English Paragraph
+var englishOne = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
+
+var englishTwo = 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+var englishThree = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words';
+
+var englishFour = 'consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.';
+
+var englishFive = 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.';
+
+
+// Hindi Paragraph
+var hindiOne = 'मानसिक हमेहो। प्राप्त भीयह कराना गोपनीयता प्रौध्योगिकी लेने ध्येय सुनत संस्था खरिदने समस्याओ समाजो ऎसाजीस आधुनिक प्रसारन अधिक हमारि ध्वनि तकनिकल व्यवहार सुनत मेंभटृ प्रव्रुति प्रोत्साहित बनाति सकती प्रमान यायेका शीघ्र पसंद जिसकी देते बिन्दुओ विश्वास किएलोग मुख्यतह निरपेक्ष पेदा हमारि';
+
+var hindiTwo = 'करने संपादक परिवहन है।अभी सादगि मुश्किले संस्क्रुति आंतरकार्यक्षमता मेमत सादगि विकास करते हुआआदी अनुवाद लाभान्वित वर्तमान संस्क्रुति कार्यसिधान्तो भीयह समाजो मुश्किल अविरोधता व्याख्यान होगा बलवान भाषा हुआआदी पत्रिका आंतरजाल चिदंश भाषाओ उसके विभाजन परिभाषित मार्गदर्शन प्राथमिक वार्तालाप शीघ्र करता कराना केन्द्रित कार्यसिधान्तो कीसे';
+
+var hindiThree = 'करता। सामूहिक कार्यलय स्वतंत्र हीकम पहोचाना सम्पर्क पसंद मजबुत बिन्दुओ कार्यलय प्रतिबध सहयोग नाकर लचकनहि दस्तावेज बनाकर किया लक्षण आशाआपस असरकारक जिसे भाषाओ आवश्यक सार्वजनिक ।क हमारी रचना खरिदने पहोचाना लिए। बनाने संस्था सारांश खरिदने दौरान आजपर अंतर्गत आंतरजाल मार्गदर्शन जानकारी व्रुद्धि वास्तविक कोहम समजते सुचना ७०है एकत्रित सम्पर्क सुचनाचलचित्र बिना ढांचा परिवहन कार्यसिधान्तो';
+
+var hindiFour = 'जाने डाले। अनुकूल किएलोग स्थापित बाजार अधिक आवश्यक गटकउसि सुनत प्रमान ब्रौशर प्रेरना हिंदी विशेष पहोचने करेसाथ अधिक स्थिति और्४५० विवरण लेने भारत पहेला भोगोलिक मुश्किले मुख्य दारी उपेक्ष वास्तविक रिती ध्वनि वेबजाल सिद्धांत लक्षण बिन्दुओ समस्याओ मार्गदर्शन करता प्रमान विकेन्द्रित लिए। डाले। अत्यंत शारिरिक ढांचा देकर';
+
+var hindiFive = 'पढांचा भारत पत्रिका विचरविमर्श वास्तव व्यवहार बीसबतेबोध कारन पुर्व पुर्णता मर्यादित सदस्य करके खरिदे द्वारा एकत्रित कम्प्युटर मानव जानते समाज कुशलता भेदनक्षमता दारी डाले। प्रतिबध लाभान्वित मुख्य खण्ड दिशामे विभाजन बीसबतेबोध माध्यम हुआआदी अतित विवरण समाजो बदले परस्पर अधिकांश अंतर्गत व्याख्यान पुर्व सुना';
+
+
+
+
+
+
+var bangla = [banglaOne, banglaTwo, banglaThree, banglaFour, banglaFive];
+var arabic = [arabicOne, arabicTwo, arabicThree, arabicFour, arabicFive];
+var english = [englishOne, englishTwo, englishThree, englishFour, englishFive];
+var hindi = [hindiOne, hindiTwo, hindiThree, hindiFour, hindiFive];
+var language = document.getElementById('language');
+var inputValue = document.getElementById('inputValue');
+var paragraph = document.getElementById('paragraph');
+var copyText = document.getElementById('copy-text');
+
+function languageFunc() {
+    if (language.value == "bangla") {
+        for (let i = 0; i < inputValue.value; i++) {
+            var p = document.createElement('p');
+            p.innerHTML = bangla[i];
+            paragraph.appendChild(p);
+        };
+    }
+    else if (language.value == "arabic") {
+        for (let i = 0; i < inputValue.value; i++) {
+            var p = document.createElement('p');
+            p.innerHTML = arabic[i];
+            paragraph.appendChild(p);
+        };
+    }
+    else if (language.value == "english") {
+        for (let i = 0; i < inputValue.value; i++) {
+            var p = document.createElement('p');
+            p.innerHTML = english[i];
+            paragraph.appendChild(p);
+        };
+    }
+    else if (language.value == "hindi") {
+        for (let i = 0; i < inputValue.value; i++) {
+            var p = document.createElement('p');
+            p.innerHTML = hindi[i];
+            paragraph.appendChild(p);
+        };
+    }
+};
+
+
+
+
+
